1c8a16240407f2f5730a3238927fc6bf16c3f1c3
afanasievM <g010203040>
2023-12-06 23:45:09 +0200

    added send version

--- src/main/kotlin/ua/com/enotagency/telegram/bot/TelegramBot.kt
+++ src/main/kotlin/ua/com/enotagency/telegram/bot/TelegramBot.kt
@@ -2,6 +2,7 @@ package ua.com.enotagency.telegram.bot
 
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.entities.ChatId
+import jakarta.annotation.PostConstruct
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.info.BuildProperties
 import org.springframework.stereotype.Component
@@ -12,7 +13,7 @@ class TelegramBot(
     private val token: String,
     @Value("\${telegram.bot.channelId}")
     private val channelId: Long,
-    buildProperties: BuildProperties
+    private val buildProperties: BuildProperties
 ) {
     private val bot = bot {
         token = this@TelegramBot.token
@@ -20,10 +21,14 @@ class TelegramBot(
 
     init {
         bot.startPolling()
-        bot.sendMessage(ChatId.fromId(channelId),"Використовується версія ${buildProperties.version}")
     }
 
     fun sendMessage(message: String) {
         bot.sendMessage(ChatId.fromId(channelId), message)
+    }
+
+    @PostConstruct
+    private fun sendVersion(){
+        bot.sendMessage(ChatId.fromId(channelId),"Використовується версія ${buildProperties.version}")
     }
 }
