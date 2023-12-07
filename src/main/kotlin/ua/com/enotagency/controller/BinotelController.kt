8c5449f166428502939fdedfcc7d6bef0fd5c0f0
afanasievM <g010203040>
2023-12-07 21:58:05 +0200

    added consumes

--- src/main/kotlin/ua/com/enotagency/controller/BinotelController.kt
+++ src/main/kotlin/ua/com/enotagency/controller/BinotelController.kt
@@ -1,6 +1,7 @@
 package ua.com.enotagency.controller
 
 import org.slf4j.LoggerFactory
+import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +13,8 @@ class BinotelController(private val channelService: ChannelService) {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
 
-    @PostMapping("/binotel/calls/incoming")
+    @PostMapping("/binotel/calls/incoming", consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+
     fun resolveIncomingBinotel(@RequestBody request: Map<String, String>): BinotelSuccessResponse {
         request.forEach{k,v-> println("$k >>>>> $v")}
 //        when{
@@ -21,7 +23,7 @@ class BinotelController(private val channelService: ChannelService) {
         return BinotelSuccessResponse()
     }
 
-    @PostMapping("/binotel/getCall/acs")
+    @PostMapping("/binotel/getCall/acs", consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
     fun getWebCalls(@RequestBody request: Map<String, String>): BinotelSuccessResponse {
         request.forEach{k,v-> println("$k >>>>> $v")}
 //        channelService.sendRequestNumber(request)
