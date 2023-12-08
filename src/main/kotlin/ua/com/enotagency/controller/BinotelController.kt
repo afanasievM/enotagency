8a699fe2f466eb286734fa81ef175eb7f12d4293
afanasievM <g010203040>
2023-12-08 23:11:16 +0200

    test different methods

--- src/main/kotlin/ua/com/enotagency/controller/BinotelController.kt
+++ src/main/kotlin/ua/com/enotagency/controller/BinotelController.kt
@@ -4,13 +4,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.ModelAttribute
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import ua.com.enotagency.dto.AnsweredTheCall
 import ua.com.enotagency.dto.BinotelSuccessResponse
 import ua.com.enotagency.dto.CallCompleted
 import ua.com.enotagency.dto.CallSetting
 import ua.com.enotagency.dto.GetCallRequest
-import ua.com.enotagency.dto.HangupTheCall
 import ua.com.enotagency.dto.ReceivedTheCall
 import ua.com.enotagency.telegram.service.ChannelService
 
@@ -19,16 +18,34 @@ class BinotelController(private val channelService: ChannelService) {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
 
+//    @PostMapping("/binotel/calls/incoming", consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+//    fun resolveIncomingBinotel(@ModelAttribute request: Any): BinotelSuccessResponse {
+//        when (request) {
+//            (request is CallSetting) -> log.info("Call setting recieved:\n${request as CallSetting}")
+//            (request is ReceivedTheCall) -> log.info("Call setting recieved:\n${request as ReceivedTheCall}")
+//            (request is CallCompleted) -> log.info("Call setting recieved:\n${request as CallCompleted}")
+//            (request is AnsweredTheCall) -> log.info("Call setting recieved:\n${request as AnsweredTheCall}")
+//            (request is HangupTheCall) -> log.info("Call setting recieved:\n${request as HangupTheCall}")
+//            else -> log.info("Unknown type \n${request.toString()}")
+//        }
+//        return BinotelSuccessResponse()
+//    }
+
     @PostMapping("/binotel/calls/incoming", consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
-    fun resolveIncomingBinotel(@ModelAttribute request: Any): BinotelSuccessResponse {
-        when (request) {
-            (request is CallSetting) -> log.info("Call setting recieved:\n${request as CallSetting}")
-            (request is ReceivedTheCall) -> log.info("Call setting recieved:\n${request as ReceivedTheCall}")
-            (request is CallCompleted) -> log.info("Call setting recieved:\n${request as CallCompleted}")
-            (request is AnsweredTheCall) -> log.info("Call setting recieved:\n${request as AnsweredTheCall}")
-            (request is HangupTheCall) -> log.info("Call setting recieved:\n${request as HangupTheCall}")
-            else -> log.info("Unknown type \n${request.toString()}")
-        }
+    fun resolveIncomingBinotel(@RequestBody request: ReceivedTheCall): BinotelSuccessResponse {
+        log.info("ReceivedTheCall recieved:\n${request}")
+        return BinotelSuccessResponse()
+    }
+
+    @PostMapping("/binotel/calls/incoming", consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    fun resolveIncomingBinotel(@RequestBody request: CallCompleted): BinotelSuccessResponse {
+        log.info("CallCompleted recieved:\n${request}")
+        return BinotelSuccessResponse()
+    }
+
+    @PostMapping("/binotel/calls/incoming", consumes = arrayOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+    fun resolveIncomingBinotel(@RequestBody request: CallSetting): BinotelSuccessResponse {
+        log.info("CallSetting recieved:\n${request}")
         return BinotelSuccessResponse()
     }
 
