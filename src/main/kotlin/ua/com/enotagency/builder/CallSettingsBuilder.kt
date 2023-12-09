a36e4f4a20129e7681e30e01a37ec822cd92739e
afanasievM <g010203040>
2023-12-09 14:19:28 +0200

    test json

--- src/main/kotlin/ua/com/enotagency/builder/CallSettingsBuilder.kt
+++ src/main/kotlin/ua/com/enotagency/builder/CallSettingsBuilder.kt
@@ -1,5 +1,6 @@
 package ua.com.enotagency.builder
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Component
 import ua.com.enotagency.dto.CallSetting
 import ua.com.enotagency.dto.enum.BinotelRequestType
@@ -8,15 +9,18 @@ import ua.com.enotagency.dto.enum.BinotelRequestType
 class CallSettingsBuilder : BinotelRequestBuilder {
     override val requestType = BinotelRequestType.API_CALL_SETTINGS
     override fun build(request: Map<String, String>): CallSetting {
-        return CallSetting(
-            pbxNumber = request["pbxNumber"],
-            externalNumber = request["externalNumber"],
-            companyID = request["companyID"],
-            callType = request["callType"],
-            language = request["language"],
-            didNumber = request["didNumber"],
-            srcNumber = request["srcNumber"],
-            requestType = requestType
-        )
+        val json = jacksonObjectMapper().writeValueAsString(request)
+        println(json)
+        return jacksonObjectMapper().readValue(json, CallSetting::class.java)
+//        return CallSetting(
+//            pbxNumber = request["pbxNumber"],
+//            externalNumber = request["externalNumber"],
+//            companyID = request["companyID"],
+//            callType = request["callType"],
+//            language = request["language"],
+//            didNumber = request["didNumber"],
+//            srcNumber = request["srcNumber"],
+//            requestType = requestType
+//        )
     }
 }
