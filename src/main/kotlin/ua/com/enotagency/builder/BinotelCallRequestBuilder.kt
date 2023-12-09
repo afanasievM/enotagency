1e3d6ca51f415fbd357698b0f537e632789cce4c
afanasievM <g010203040>
2023-12-09 13:37:17 +0200

    test injection

--- src/main/kotlin/ua/com/enotagency/builder/BinotelCallRequestBuilder.kt
+++ src/main/kotlin/ua/com/enotagency/builder/BinotelCallRequestBuilder.kt
@@ -1,21 +1,21 @@
 package ua.com.enotagency.builder
 
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Component
 import ua.com.enotagency.dto.BinotelCallRequest
 
-@Component
 object BinotelCallRequestBuilder {
-    @Autowired
-    lateinit var builders: List<BinotelRequestBuilder>
+
+    private var builders: List<BinotelRequestBuilder> = mutableListOf()
+
+    fun init(builders:List<BinotelRequestBuilder>){
+        this.builders = builders
+    }
 
     fun build(request: Map<String, String>): BinotelCallRequest? {
         return choose(request)?.let { build(request) }
-
     }
 
     private fun choose(request: Map<String, String>): BinotelRequestBuilder? {
         val requestType = request["requestType"]
-        return builders.filter { it.requestType.toString() == requestType }.first
+        return builders.firstOrNull { it.requestType.toString() == requestType }
     }
 }
---
+++ src/main/kotlin/ua/com/enotagency/config/AppConfig.kt
@@ -0,0 +1,14 @@
+package ua.com.enotagency.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import ua.com.enotagency.builder.BinotelCallRequestBuilder
+import ua.com.enotagency.builder.BinotelRequestBuilder
+@Configuration
+class AppConfig {
+    @Bean
+    fun binotelCallRequestBuilder(builders: List<BinotelRequestBuilder>): BinotelCallRequestBuilder {
+        BinotelCallRequestBuilder.init(builders)
+        return BinotelCallRequestBuilder
+    }
+}
