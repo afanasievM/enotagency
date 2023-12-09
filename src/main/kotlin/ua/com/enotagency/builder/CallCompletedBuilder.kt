ca08c4f2b59cd647d09b49a7d84160af10d823ee
afanasievM <g010203040>
2023-12-09 20:52:03 +0200

    test call

--- src/main/kotlin/ua/com/enotagency/builder/CallCompletedBuilder.kt
+++ src/main/kotlin/ua/com/enotagency/builder/CallCompletedBuilder.kt
@@ -1,25 +1,25 @@
-package ua.com.enotagency.builder
-
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.google.gson.Gson
-import org.springframework.stereotype.Component
-import ua.com.enotagency.dto.CallCompleted
-import ua.com.enotagency.dto.enum.BinotelRequestType
-
-@Component
-class CallCompletedBuilder : BinotelRequestBuilder {
-    override val requestType = BinotelRequestType.API_CALL_COMPLETED
-
-    private val gson = Gson()
-
-    override fun build(request: Map<String, String>): CallCompleted {
-        val json = jacksonObjectMapper().writeValueAsString(request)
-        println(json)
-        return CallCompleted(
-            language = request["language"],
-            requestType = requestType,
-            callDetails = null,
-            attemptsCounter = request["attemptsCounter"]
-        )
-    }
-}
+//package ua.com.enotagency.builder
+//
+//import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+//import com.google.gson.Gson
+//import org.springframework.stereotype.Component
+//import ua.com.enotagency.dto.CallCompleted
+//import ua.com.enotagency.dto.enum.BinotelRequestType
+//
+//@Component
+//class CallCompletedBuilder : BinotelRequestBuilder {
+//    override val requestType = BinotelRequestType.API_CALL_COMPLETED
+//
+//    private val gson = Gson()
+//
+//    override fun build(request: Map<String, String>): CallCompleted {
+//        val json = jacksonObjectMapper().writeValueAsString(request)
+//        println(json)
+//        return CallCompleted(
+//            language = request["language"],
+//            requestType = requestType,
+//            callDetails = null,
+//            attemptsCounter = request["attemptsCounter"]
+//        )
+//    }
+//}
