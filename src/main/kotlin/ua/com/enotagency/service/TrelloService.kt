b9c1e2a2cc0a66d207f0d78fafdc037aaccf3397
afanasievM <g010203040>
2023-12-10 18:17:02 +0200

    changed workwlow

--- src/main/kotlin/ua/com/enotagency/service/TrelloService.kt
+++ src/main/kotlin/ua/com/enotagency/service/TrelloService.kt
@@ -42,7 +42,6 @@ class TrelloService(private val trelloClient: Trello) {
             val newCard = Card()
             newCard.name = externalNumber
             newCard.idBoard = boardId
-            newCard.pos=2
             trelloClient.createCard(callListId, newCard)
         }
     }
