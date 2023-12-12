171aa084308c75d635bdaf2d0c43fc16f94e874e
afanasievM <g010203040>
2023-12-12 22:26:08 +0200

    all cards before by Id

--- src/main/kotlin/ua/com/enotagency/service/TrelloService.kt
+++ src/main/kotlin/ua/com/enotagency/service/TrelloService.kt
@@ -52,8 +52,8 @@ class TrelloService(private val trelloClient: Trello) {
 
     fun getCardById(cardId: String) {
         log.info("cardId:$cardId")
-        val card = trelloClient.getBoardCard(boardId, cardId)
         getBoard().fetchCards().forEach { println(it.id) }
+        val card = trelloClient.getBoardCard(boardId, cardId)
         log.info(jacksonObjectMapper().writeValueAsString(card))
     }
 
