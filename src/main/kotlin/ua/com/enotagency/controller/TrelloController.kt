df7640225ca6888683675a6929396016e90eb25a
afanasievM <g010203040>
2023-12-12 21:54:05 +0200

    try requestParam

--- src/main/kotlin/ua/com/enotagency/controller/TrelloController.kt
+++ src/main/kotlin/ua/com/enotagency/controller/TrelloController.kt
@@ -4,7 +4,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import ua.com.enotagency.executor.Executor
@@ -18,7 +18,7 @@ class TrelloController(
 
     @PostMapping("trello/card/horse", consumes = arrayOf(MediaType.ALL_VALUE))
     @ResponseStatus(HttpStatus.OK)
-    fun resolveIncomingBinotel(@RequestBody cardId: String) {
+    fun resolveIncomingBinotel(@RequestParam cardId: String) {
         log.info("Recieved:\n$cardId")
         Executor.execute { trelloService.getCardById(cardId) }
     }
